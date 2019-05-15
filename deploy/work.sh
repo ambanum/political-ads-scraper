b6cf0b4a43d20f1014ed793ff69254043e783f68
Michel Blancard <github@michelblancard.fr>
2019-05-15 17:14:26 +0200

    Fix deploy script

--- deploy/work.sh
+++ deploy/work.sh
@@ -3,7 +3,8 @@ set -e
 
 ### Configuration ###
 
-APP_DIR=/home/cloud/political-ads-scraper
+REPO_DIR=/home/cloud/political-ads-scraper
+APP_DIR=/home/cloud/political-ads-scraper/srv
 GIT_URL=git@github.com:ambanum/political-ads-scraper.git
 
 ### Automation steps ###
@@ -12,15 +13,15 @@ set -x
 
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa_deploy_key
 # Pull latest code
-if [[ -e $APP_DIR ]]; then
-  cd $APP_DIR
+if [[ -e $REPO_DIR ]]; then
+  cd $REPO_DIR
   git pull
 else
-  git clone $GIT_URL $APP_DIR
-  cd $APP_DIR
+  git clone $GIT_URL $REPO_DIR
 fi
 
 # Install dependencies
+cd $APP_DIR
 npm install --production
 npm prune --production
 
