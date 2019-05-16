b66f911f78f1adf8d999891ab00cb3f25f7ab508
Nicolas Dupont <npg.dupont@gmail.com>
2019-05-16 14:54:05 +0200

    Use cors

--- srv/src/app.js
+++ srv/src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const router = require('./router');
 
-
 const app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', router);
 
