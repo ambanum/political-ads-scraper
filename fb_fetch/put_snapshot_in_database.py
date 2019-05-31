426ea7768329901d80c141f5fe7eb1d00917b48c
Michel Blancard <git@michelblancard.fr>
2019-05-31 09:52:55 +0200

    Recover on errors when fetching images

--- fb_fetch/put_snapshot_in_database.py
+++ fb_fetch/put_snapshot_in_database.py
@@ -4,6 +4,7 @@ ssh -NT -L 27018:localhost:27017 cloud@desinfo.quaidorsay.fr
 
 
 import time
+import logging
 
 from pymongo import MongoClient
 
@@ -13,34 +14,50 @@ from fb_fetch import snapshot
 from fb_fetch.fb_login import login
 
 
-client = MongoClient(config.MONGODB_URL)
-ads_collection = client.facebook_ads.ads
+def process_batch():
 
-#ads_collection.create_index('ad_id')
+    client = MongoClient(config.MONGODB_URL)
+    ads_collection = client.facebook_ads.ads
 
+    #ads_collection.create_index('ad_id')
 
-user_access_token, browser = login.connect_and_get_user_token()
+    user_access_token, browser = login.connect_and_get_user_token()
 
-ads_to_process = ads_collection.find({ "snapshot" : { "$exists" : False } })
+    ads_to_process = ads_collection.find({ "snapshot" : { "$exists" : False } })
 
-for ad in ads_to_process:
-    ad_id = fetch.get_ad_id(ad)
-    print(ad_id)
+    for ad in ads_to_process:
+        ad_id = fetch.get_ad_id(ad)
+        print(ad_id)
 
-    snapshot_data = snapshot.get_snapshot_data(
-        user_access_token=user_access_token,
-        browser=browser,
-        ad_id=ad_id,
-    )
+        try:
+            snapshot_data = snapshot.get_snapshot_data(
+                user_access_token=user_access_token,
+                browser=browser,
+                ad_id=ad_id,
+            )
 
-    ads_collection.update_one(
-        {'_id': ad['_id'] },
-        {
-            '$set': {
-                'ad_id': ad_id,
-                'snapshot': snapshot_data,
-            },
-        },
-    )
-    
-    time.sleep(4)
+            ads_collection.update_one(
+                {'_id': ad['_id'] },
+                {
+                    '$set': {
+                        'ad_id': ad_id,
+                        'snapshot': snapshot_data,
+                    },
+                },
+            )
+
+            time.sleep(4)
+
+        except Exception:
+            logging.exception('Something happened')
+            time.sleep(5)
+            #user_access_token, browser = login.connect_and_get_user_token()
+
+process_batch()
+#while True:
+#    try:
+#        process_batch()
+
+#    except Exception:
+#        logging.exception('Something happened')
+#        time.sleep(5)
