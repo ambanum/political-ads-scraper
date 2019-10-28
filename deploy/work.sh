4e128189f9b92d3cde22d63d0cb710a2c65ea30e
Michel Blancard <github@michelblancard.fr>
2019-10-28 13:05:37 +0100

    Fix deploy script

--- deploy/work.sh
+++ deploy/work.sh
@@ -26,4 +26,4 @@ npm install --production
 npm prune --production
 
 # Restart app
-forever restart bin/run.js || forever start bin/run.js
+forever restart srv/bin/run.js || forever start srv/bin/run.js
